c0fcb0ff97c366735c65fdbfd8f729b8d4a4b233
Eyk Rehbein <eykrehbein@gmail.com>
2019-12-19 11:01:19 +0100

    add datepicker

--- src/components/EditableValue/EditableValue.js
+++ src/components/EditableValue/EditableValue.js
@@ -24,11 +24,90 @@ export default class EditableValue extends Component {
     e.target.parentElement.parentElement.querySelector('input').select();
   }
 
+  // function that fires on input key down - date picker
+  inputKeyPressed(e) {
+    const key = e.key;
+
+    const numbersOnlyRegex = RegExp('^[0-9]*$', 'gm');
+    if (this.props.isDatePicker) {
+      if (numbersOnlyRegex.test(key) === false) {
+        e.preventDefault();
+        return;
+      }
+      const currentValue = e.target.value;
+
+      const zeroToThreeRegex = RegExp('^[0-3]*$', 'gm');
+      const zeroToOneRegex = RegExp('^[0-1]*$', 'gm');
+      const zeroToTwoRegex = RegExp('^[0-2]*$', 'gm');
+      const oneToTwoRegex = RegExp('^[1-2]*$', 'gm');
+
+      if (currentValue.length === 0) {
+        if (zeroToThreeRegex.test(key) === false) {
+          e.preventDefault();
+          return;
+        }
+      }
+
+      if (currentValue.length === 3) {
+        if (zeroToOneRegex.test(key) === false) {
+          e.preventDefault();
+          return;
+        }
+      }
+
+      if (currentValue.length === 6) {
+        if (oneToTwoRegex.test(key) === false) {
+          e.preventDefault();
+          return;
+        }
+      }
+
+      if (currentValue.length === 4) {
+        if (currentValue.split('')[3] === '1') {
+          if (zeroToTwoRegex.test(key) === false) {
+            e.preventDefault();
+            return;
+          }
+        }
+      }
+      if (currentValue.length === 7) {
+        if (currentValue.split('')[6] === '2') {
+          if (key !== '0') {
+            e.preventDefault();
+            return;
+          }
+        }
+        if (currentValue.split('')[6] === '1') {
+          if (key !== '9') {
+            e.preventDefault();
+            return;
+          }
+        }
+      }
+
+      if (currentValue.length === 1 || currentValue.length === 4) {
+        e.preventDefault();
+        e.target.value = currentValue + key + '.';
+      }
+
+      if (currentValue.length === 2 || currentValue.length === 5) {
+        e.preventDefault();
+        e.target.value = currentValue + '.' + key;
+      }
+
+      if (currentValue.length >= 10) {
+        e.preventDefault();
+      }
+    }
+  }
+
+  // function that fires on input change
   inputValueChanged(e) {
     const val = e.target.value;
-    localStorage.setItem(`cpj:${this.props.inputClassifier}`, val);
 
+    localStorage.setItem(`cpj:${this.props.inputClassifier}`, val);
     this.setState({ inputValue: val });
+    return;
   }
 
   // unfocus input when user hits enter
@@ -45,6 +124,7 @@ export default class EditableValue extends Component {
           <input
             value={this.state.inputValue}
             onChange={this.inputValueChanged.bind(this)}
+            onKeyPress={this.inputKeyPressed.bind(this)}
           ></input>
           <div className="editIcon">
             <i
