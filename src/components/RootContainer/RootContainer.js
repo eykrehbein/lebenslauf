7f0c5edd92335d59e8ff80249d9fdb88f2cd58c3
Eyk Rehbein <eykrehbein@gmail.com>
2019-12-19 09:22:30 +0100

    initial commit

---
+++ src/components/RootContainer/RootContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import './RootContainer.scss';
+
+export default class RootContainer extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+  render() {
+    return <div className="rootContainer grid h-center">test</div>;
+  }
+}
